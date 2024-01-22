25e32dd4fef4dd3d6b3f4829b0c3f9a297db4b1a
irisacsee <2277209447@qq.com>
2024-01-22 17:03:07 +0800

    fix: HttpResponseUtil

--- GeoDataServer/src/main/java/cn/edu/whu/gds/util/HttpResponseUtil.java
+++ GeoDataServer/src/main/java/cn/edu/whu/gds/util/HttpResponseUtil.java
@@ -5,11 +5,13 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.ServletResponse;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
+@Component
 public class HttpResponseUtil {
     public Response responseRender(HttpStatus httpStatus, String msg) {
         return new Response(httpStatus.value(), msg);
