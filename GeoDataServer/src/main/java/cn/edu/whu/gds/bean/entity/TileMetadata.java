1e7418694e608919f19ede077e5127be028319ee
“pineappleBBL” <1106167237@qq.com>
2024-01-24 20:57:37 +0800

    feat:add annotations for Tilemetadata

--- GeoDataServer/src/main/java/cn/edu/whu/gds/bean/entity/TileMetadata.java
+++ GeoDataServer/src/main/java/cn/edu/whu/gds/bean/entity/TileMetadata.java
@@ -4,12 +4,18 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.sql.Timestamp;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "ea_tile")
 public class TileMetadata {
+    @Id
     private Integer id;
     private String name;
     private String path;
