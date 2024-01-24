cf7d31a43f8281ab026a91cc6f38d0b48290ce03
“pineappleBBL” <1106167237@qq.com>
2024-01-24 21:02:17 +0800

    feat: add an interface to obtain tile metadata

---
+++ GeoDataServer/src/main/java/cn/edu/whu/gds/service/TileMetadataRepository.java
@@ -0,0 +1,12 @@
+package cn.edu.whu.gds.service;
+
+import cn.edu.whu.gds.bean.entity.TileMetadata;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TileMetadataRepository extends CrudRepository<TileMetadata, Long> {
+
+    // 根据name属性查询数据
+    @Query("SELECT t FROM TileMetadata t WHERE t.name = ?1")
+    TileMetadata findBySql(String name);
+}
